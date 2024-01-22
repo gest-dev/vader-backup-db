3a26709801e737283fdc3ff9c5cbda8d057009d2
gezerramos <gezerramo@gmail.com>
2024-01-22 19:47:32 -0300

    Add contabo comp aws

--- src/MongoDump.js
+++ src/MongoDump.js
@@ -1,11 +1,13 @@
 
 const { exec } = require('child_process');
-const ftp = require('basic-ftp');
 const { format } = require('date-fns');
 const fs = require('fs').promises;
 const path = require('path');
+
 const { senMessageTelegran } = require('./Services/sendTelegramAlert')
-const AWS = require('aws-sdk');
+const { uploadToFTP } = require('./Services/FTP');
+const { uploadToSW3 } = require('./Services/S3Aws');
+const { uploadToContabo } = require('./Services/ContaboAws');
 
 
 // Credenciais para conexão com o banco de dados
@@ -57,93 +59,7 @@ function execCommand(command, operation) {
     });
 }
 
-async function uploadToFTP(localFilePath, remoteFilePath) {
 
-    try {
-        const ftpConfig = {
-            host: process.env.FTP_HOST,
-            user: process.env.FTP_USER,
-            port: process.env.FTP_PORT,
-            password: process.env.FTP_PASS,
-        };
-
-        // Conectar ao servidor FTP
-        const client = new ftp.Client();
-        await client.access(ftpConfig);
-
-        // Enviar arquivo para o servidor FTP
-        await client.uploadFrom(localFilePath, remoteFilePath);
-        // Fechar conexão FTP
-
-
-        console.log(`Upload concluído com sucesso: ${remoteFilePath}`);
-    } catch (error) {
-        console.error(`Erro durante o upload para o FTP: ${error.message}`);
-        throw error;
-    } finally {
-        // Fechar conexão FTP, independentemente do resultado
-        await client.close();
-    }
-}
-
-async function uploadToSW3(localFilePath, remoteFilePath) {
-    try {
-        // Configurar as credenciais AWS (configure-as com suas próprias credenciais)
-        AWS.config.update({
-            accessKeyId: 'SUA_ACCESS_KEY',
-            secretAccessKey: 'SUA_SECRET_KEY',
-            region: 'SUA_REGIAO', // por exemplo, 'us-east-1'
-        });
-
-        const s3 = new AWS.S3();
-
-        // Parâmetros para upload
-        const params = {
-            Bucket: 'NOME_DO_SEU_BUCKET',
-            Key: remoteFilePath,
-            Body: await fs.readFile(localFilePath),
-        };
-
-        // Realizar o upload para o Amazon S3
-        const result = await s3.upload(params).promise();
-
-        console.log(`Upload para Amazon S3 concluído com sucesso: ${result.Location}`);
-    } catch (error) {
-        console.error(`Erro durante o upload para Amazon S3: ${error.message}`);
-        throw error;
-    }
-}
-
-async function uploadToContabo(localFilePath, remoteFilePath) {
-    try {
-        // Configurar as credenciais Contabo (substitua com suas próprias credenciais)
-        AWS.config.update({
-            accessKeyId: process.env.CONTABO_ACCESS_KEY_ID,
-            secretAccessKey: process.env.CONTABO_SECRET_ACCESS_KEY,
-            region: process.env.CONTABO_REGION, // substitua pela região apropriada
-        });
-
-        const s3 = new AWS.S3({
-            endpoint: process.env.CONTABO_ENDPOINT,
-            s3ForcePathStyle: true,
-        });
-
-        // Parâmetros para upload
-        const params = {
-            Bucket: process.env.CONTABO_BUCKET, // substitua com o nome do bucket da Contabo
-            Key: remoteFilePath,
-            Body: await fs.readFile(localFilePath),
-        };
-
-        // Realizar o upload para a Contabo Storage
-        const result = await s3.upload(params).promise();
-        return result;
-
-    } catch (error) {
-        console.error(`Erro durante o upload para Contabo Storage: ${error.message}`);
-        throw error;
-    }
-}
 
 exports.exeMongodump = async () => {
 
---
+++ src/Services/FTP.js
@@ -0,0 +1,31 @@
+const ftp = require('basic-ftp');
+
+async function uploadToFTP(localFilePath, remoteFilePath) {
+
+    try {
+        const ftpConfig = {
+            host: process.env.FTP_HOST,
+            user: process.env.FTP_USER,
+            port: process.env.FTP_PORT,
+            password: process.env.FTP_PASS,
+        };
+
+        // Conectar ao servidor FTP
+        const client = new ftp.Client();
+        await client.access(ftpConfig);
+
+        // Enviar arquivo para o servidor FTP
+        await client.uploadFrom(localFilePath, remoteFilePath);
+        // Fechar conexão FTP
+
+
+        console.log(`Upload concluído com sucesso: ${remoteFilePath}`);
+    } catch (error) {
+        console.error(`Erro durante o upload para o FTP: ${error.message}`);
+        throw error;
+    } finally {
+        // Fechar conexão FTP, independentemente do resultado
+        await client.close();
+    }
+}
+exports.uploadToFTP = uploadToFTP;

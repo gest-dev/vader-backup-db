5aacda2ba00ae0e9a0c492b10d74f5030fece8e4
Gezer <gezer@inventotecnologia.com.br>
2024-09-23 19:49:22 -0300

    atualizando pacotes

--- app-dump.js
+++ app-dump.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
-const cronJob = require("cron").job;
+const { CronJob } = require("cron");
 const { exeDBDump } = require("./src/Controllers/DBDumpController");
 
-exeDBDump();
-cronJob(
-    `${process.env.CRON_TIME || "1 1 */6 * * *"}`,// Default is every 6 hours
-    () => {
-        exeDBDump();
-    },
-    null,
-    true
+//exeDBDump();
+const job = new CronJob(
+    `${process.env.CRON_TIME || "1 1 */6 * * *"}`,
+    exeDBDump(), // The function to execute
+    null, // On complete function
+    true, // Start the job right now
+    "America/Sao_Paulo"
 );
+//job.start(); // Start the job right now

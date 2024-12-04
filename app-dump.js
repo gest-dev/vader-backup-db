581f6d849b4d4228f4259e3b6c4258cf0b521c65
Gezer <gezer@inventotecnologia.com.br>
2024-12-04 18:27:41 -0300

    correção chemar função exeDBDump

--- app-dump.js
+++ app-dump.js
@@ -4,7 +4,7 @@ const { exeDBDump } = require("./src/Controllers/DBDumpController");
 
 const job = new CronJob(
     process.env.CRON_TIME || "0 */3 * * *", // Padrão de tempo
-    exeDBDump(),
+    exeDBDump, // Passar como referência, sem os parênteses
     null, // Função executada ao completar o cron (opcional)
     true, // Iniciar automaticamente
     "America/Sao_Paulo" // Fuso horário

ad7deefe85c4aaa884a9ce8c1c43713c47d5ab24
Gezer <gezer@inventotecnologia.com.br>
2024-12-04 15:14:32 -0300

    cron error function

--- app-dump.js
+++ app-dump.js
@@ -3,16 +3,8 @@ const { CronJob } = require("cron");
 const { exeDBDump } = require("./src/Controllers/DBDumpController");
 
 const job = new CronJob(
-    process.env.CRON_TIME || "1 1 */6 * * *", // Padrão de tempo
-    async () => {
-        try {
-            console.log("Iniciando backup...");
-            await exeDBDump(); // Executa a função como parte do cron job
-            console.log("Backup concluído.");
-        } catch (error) {
-            console.error("Erro ao executar o backup:", error);
-        }
-    },
+    process.env.CRON_TIME || "0 */3 * * *", // Padrão de tempo
+    exeDBDump(),
     null, // Função executada ao completar o cron (opcional)
     true, // Iniciar automaticamente
     "America/Sao_Paulo" // Fuso horário

e089b5cfa823eee0e407fab8d41d6abc1e08dcde
Gezer <gezer@inventotecnologia.com.br>
2024-12-04 15:05:46 -0300

    cron error function

--- app-dump.js
+++ app-dump.js
@@ -2,12 +2,19 @@ require("dotenv").config();
 const { CronJob } = require("cron");
 const { exeDBDump } = require("./src/Controllers/DBDumpController");
 
-// Ajuste no CronJob
 const job = new CronJob(
-    `${process.env.CRON_TIME || "1 1 */6 * * *"}`, // Padrão de tempo
-    exeDBDump, // Passe a função como referência, sem parênteses
-    null, // Função de callback "onComplete"
-    true, // Inicia o job automaticamente
+    `${process.env.CRON_TIME || "1 1 */6 * * *"}`, // Padrão de agendamento
+    async () => {
+        try {
+            await exeDBDump(); // Aguarde a execução da função assíncrona
+        } catch (error) {
+            console.error("Erro no cron job:", error);
+        }
+    },
+    null, // Função para "onComplete"
+    true, // Iniciar o cron automaticamente
     "America/Sao_Paulo" // Fuso horário
 );
-//job.start(); // Start the job right now
+
+// O cron job já será iniciado automaticamente devido ao parâmetro `true`
+// job.start(); // Opcional, já iniciado automaticamente
